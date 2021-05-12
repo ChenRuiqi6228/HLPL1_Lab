fb517a04713db544405d1e5e92a9a7d7d47e52ed
ChenRuiqi6228 <81504470+ChenRuiqi6228@users.noreply.github.com>
2021-05-12 11:37:32 +0800

    Update Drill 18.1.cpp

--- Drill 18.1.cpp
+++ Drill 18.1.cpp
@@ -8,42 +8,42 @@
 
     void f(int a[], int n)
 {
-    //3.1: Define a local int array la of ten ints.
+    //3.1a: Define a local int array la of ten ints.
 
 	int la[10];
 
-    //3.2: Copy the values from ga into la.
+    //3.2b: Copy the values from ga into la.
 
 	for (int i = 0; i < n; ++i)
         la[i] = ga[i];
 
-    //3.3: Print out the elements of la.
+    //3.3c: Print out the elements of la.
 
     for (int i = 0; i < n; ++i)
         cout << la[i] << "  ";
     cout << endl;
 
-    //3.4: Define a pointer p to int and initialize it with an array allocated on the free store with the same number of elements as the argument array.
+    //3.4d: Define a pointer p to int and initialize it with an array allocated on the free store with the same number of elements as the argument array.
    
     int* p = new int[n];
-    //3.5: Copy the values from the argument array into the free-store array.
+    //3.5e: Copy the values from the argument array into the free-store array.
     
     for (int i = 0; i < n; ++i)
         p[i] = a[i];
 
-    //3.6: Print out the elements of the free-store array.
+    //3.6f: Print out the elements of the free-store array.
     for (int i = 0; i < n; ++i)
         cout << p[i] << "  ";
     cout << endl;
 
-    //3.7: Deallocate the free-store array.
+    //3.7g: Deallocate the free-store array.
     delete[] p;
 }
 int main()
 { 
-    //4.1: Call f() with ga as its argument.
+    //4.1a: Call f() with ga as its argument.
     f(ga, 10);
-    //4.2: Define an array aa with ten elements, and initialize it with the first ten factorial values (1, 2*1, 3*2*1, 4*3*2*1, etc.).
+    //4.2b: Define an array aa with ten elements, and initialize it with the first ten factorial values (1, 2*1, 3*2*1, 4*3*2*1, etc.).
     int aa[10];
     int n = 1;
     for (int i = 0; i < 10; i++)
@@ -51,7 +51,7 @@ int main()
         n *= (i + 1);
         aa[i] = n;
     }
-    //c. Call f() with aa as its argument.
+    //4.3c: Call f() with aa as its argument.
     f(aa, 10);
 
     return 0;
